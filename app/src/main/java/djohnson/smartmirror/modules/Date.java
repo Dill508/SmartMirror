430248d9a7997e2e6cc26829c8023ded1c1e982f
Dillon Johnson <dilljohnson508@gmail.com>
2016-05-14 23:27:39 -0500

    Added Date class

---
+++ app/src/main/java/djohnson/smartmirror/modules/Date.java
@@ -0,0 +1,23 @@
+package djohnson.smartmirror.modules;
+
+import android.text.Html;
+import android.text.Spanned;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+/**
+ * Created by Dillon on 5/14/2016.
+ *
+ */
+public class Date {
+    public static Spanned getDate() {
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.US);
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd", Locale.US);
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.US);
+        Calendar now = Calendar.getInstance();
+        //int dayOfMonth = now.get(Calendar.DAY_OF_MONTH);
+        return Html.fromHtml(monthFormat.format(now.getTime()) + " " + dayFormat.format(now.getTime()) + ", " + yearFormat.format(now.getTime()));
+    }
+}
