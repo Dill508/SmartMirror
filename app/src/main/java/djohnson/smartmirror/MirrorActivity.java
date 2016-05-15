c5c038a51cb5b24f34ee9cbe1d4f42a799f8bda9
Dillon Johnson <dilljohnson508@gmail.com>
2016-05-14 19:34:07 -0500

    Initial commit

---
+++ app/src/main/java/djohnson/smartmirror/MirrorActivity.java
@@ -0,0 +1,13 @@
+package djohnson.smartmirror;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MirrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mirror);
+    }
+}
