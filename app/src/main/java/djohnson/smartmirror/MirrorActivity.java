d750eeaac636da6bdf8451d8ccd473d75dcebd8c
Dillon Johnson <dilljohnson508@gmail.com>
2016-05-14 20:00:35 -0500

    Added Date and Time

--- app/src/main/java/djohnson/smartmirror/MirrorActivity.java
+++ app/src/main/java/djohnson/smartmirror/MirrorActivity.java
@@ -1,13 +1,52 @@
 package djohnson.smartmirror;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import djohnson.smartmirror.modules.Date;
 
 public class MirrorActivity extends AppCompatActivity {
 
+    private TextView currentDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mirror);
+
+        //Sets to fullscreen and removes notification and action bar
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }else {
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.hide();
+
+            currentDate = (TextView) findViewById(R.id.date_text);
+
+            updateMirrorInfo();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        updateMirrorInfo();
+    }
+
+    private void updateMirrorInfo() {
+        currentDate.setText(Date.getDate());
     }
 }
