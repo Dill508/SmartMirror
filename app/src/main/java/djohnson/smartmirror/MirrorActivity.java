050bd16abce1fb70e259b42b592a6c20ad3fb9af
Dillon Johnson <dilljohnson508@gmail.com>
2016-05-14 23:22:27 -0500

    Added Forecast.io API key

--- app/src/main/java/djohnson/smartmirror/MirrorActivity.java
+++ app/src/main/java/djohnson/smartmirror/MirrorActivity.java
@@ -5,7 +5,6 @@ import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
