4d31c579356054bec7bee6022b9b7b9913fec222
Dillon Johnson <dilljohnson508@gmail.com>
2016-05-21 18:49:53 -0500

    minor cleanup

--- app/src/main/java/djohnson/smartmirror/MirrorActivity.java
+++ app/src/main/java/djohnson/smartmirror/MirrorActivity.java
@@ -114,6 +114,7 @@ public class MirrorActivity extends AppCompatActivity {
         };
         timer.schedule(timerTask, 0, 5 * 60 * 1000);
 
+
 //            Handler handler = new Handler();
 //            handler.postDelayed(new Runnable() {
 //                @Override
